9a19bc1c083885dadcfd9348e9767d845faa85a0
Vladyslav Bukreiev <bukerman@gmail.com>
2018-12-24 22:37:15 +0200

    8

--- blog/views.py
+++ blog/views.py
@@ -50,7 +50,7 @@ class DraftListView(LoginRequiredMixin,ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('-created_date')
 
 
 class PostDeleteView(LoginRequiredMixin,DeleteView):
@@ -80,6 +80,20 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html', {'form': form})
+
+@login_required
+def comment_edit(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    # if request.method == "POST":
+    form = CommentForm(request.POST)
+    #     if form.is_valid():
+    #         comment = form.save(commit=False)
+    #         comment.post = post
+    #         comment.save()
+            # return redirect('post_detail', pk=comment.post.pk)
+    # else:
+         # form = CommentForm()
+    return render(request, 'blog/comment_edit.html', {'form': form})
 
 
 @login_required
