359c9f0174093ecc5a640ac84a1c8b6c7e0cd827
Vladyslav Bukreiev <bukerman@gmail.com>
2018-12-25 15:14:47 +0200

    10

--- blog/views.py
+++ blog/views.py
@@ -69,7 +69,6 @@ def post_publish(request, pk):
 
 @login_required
 def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -83,19 +82,18 @@ def add_comment_to_post(request, pk):
 
 @login_required
 def comment_edit(request, pk):
-    form = CommentForm()
+    # post = get_object_or_404(Post, pk=pk)
     comment = get_object_or_404(Comment, pk=pk)
-    print(str(comment.pk))
-    print(comment)
-    # comment.post = post
 
     if request.method == "POST":
-        # form = CommentForm(data=request.POST)
+        form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = post
+            # comment.post = post
             comment.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_detail', pk=comment.post.pk)
+    else:
+        form = CommentForm(instance=comment)
     return render(request, 'blog/comment_edit.html', {'form': form})
 
 
